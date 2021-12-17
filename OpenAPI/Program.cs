a57f7fd9e544de838d96c81e1d8609a3326c6979
Sveinung Himle <himsve@kartverket.no>
2021-12-17 16:14:37 +0100

    OpenApi client works

--- OpenAPI/Program.cs
+++ OpenAPI/Program.cs
@@ -6,10 +6,16 @@ namespace OpenAPI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            var oa = new OpenAPI.openapiClient(new System.Net.Http.HttpClient());
+            Console.WriteLine("Transformation is started");
 
-           var getSync = oa.GetAsync();
+            using var httpClient = new System.Net.Http.HttpClient();
+            var oa = new OpenAPI.TransformerClient(httpClient);
+            
+            for (int i = 0; i < 100; i++)
+            {                
+                var res = oa.GetAsync(7d + (double)(i /100000d), 60d + (double)(i / 100000d), OpenAPI.Epsg._4258, OpenAPI.Epsg._25832).Result;
+            }
+            Console.WriteLine("Finish!");
         }
     }
 }
