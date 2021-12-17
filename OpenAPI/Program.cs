a61463ccfc2e748639a52d32c7a8cd4891fc2da7
Sveinung Himle <himsve@kartverket.no>
2021-12-17 14:49:58 +0100

    Adding OpenApi client

---
+++ OpenAPI/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OpenAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var oa = new OpenAPI.openapiClient(new System.Net.Http.HttpClient());
+
+           var getSync = oa.GetAsync();
+        }
+    }
+}
